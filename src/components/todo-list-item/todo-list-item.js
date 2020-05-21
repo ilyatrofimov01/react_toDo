287daa7b19aab18abdc42511d4f7bf9df4dc20b0
Ilya <ilya.trofimov.01@gmail.com>
2020-05-21 21:32:04 +0300

    added classes

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './todo-list-item.css';
 
-const ToDoListItem = ({ label, important = false }) =>{
+export default class ToDoTilstItem extends Component{
+    render (){
+        const { label, important = false } = this.props;
+        const style ={
+            color: important? 'steelblue' : 'black',
+            fontWeight: important? 'bold' : 'normal'
+        }
+    return (
+            <span className="todo-list-item">
+                <span
+                     className="todo-list-item-label"
+                    style={style}>
+                     {label}
+                </span>
+        
+                <button type="button"
+                    className="btn btn-outline-success btn-sm float-right">
+                     <i className="fa fa-exclamation" />
+                </button>
+        
+                <button type="button"
+                    className="btn btn-outline-danger btn-sm float-right">
+                     <i className="fa fa-trash-o" />
+                </button>
+        
+          </span>
+        )
+    };
 
-    const style ={
-        color: important? 'steelblue' : 'black',
-        fontWeight: important? 'bold' : 'normal'
-    }
+}
 
 
-    
-return (
-    <span className="todo-list-item">
-        <span
-             className="todo-list-item-label"
-            style={style}>
-             {label}
-        </span>
 
-        <button type="button"
-            className="btn btn-outline-success btn-sm float-right">
-             <i className="fa fa-exclamation" />
-        </button>
 
-        <button type="button"
-            className="btn btn-outline-danger btn-sm float-right">
-             <i className="fa fa-trash-o" />
-        </button>
-
-  </span>
-)};
-
-export default  ToDoListItem; 
