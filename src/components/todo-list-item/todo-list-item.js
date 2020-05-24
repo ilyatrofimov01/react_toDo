2945919d5507bc7124e6f401f562b59b4b281440
Ilya <ilya.trofimov.01@gmail.com>
2020-05-24 03:21:28 +0300

    onclick

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class ToDoTilstItem extends Component{
+
+   onLabelClick =() => {
+            console.log(`Done ${this.props.label}`);
+        }
+
     render (){
         const { label, important = false } = this.props;
         const style ={
@@ -12,8 +17,11 @@ export default class ToDoTilstItem extends Component{
             <span className="todo-list-item">
                 <span
                      className="todo-list-item-label"
-                    style={style}>
+                    style={style}
+                    onClick = {this.onLabelClick }
+                    >
                      {label}
+                     
                 </span>
         
                 <button type="button"
